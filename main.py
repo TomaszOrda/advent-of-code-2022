3fd3a9a1b6c1cdd8d35db48b3ff35766f90ec1c6
TomaszOrda <tomaszorda@outlook.com>
2025-01-02 20:17:19 +0100

    Add main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+import importlib
+import sys
+
+
+if __name__ == "__main__":
+    DAY, PART = sys.argv[1].removeprefix("day").split("part")
+    # TEST = "-test" in sys.argv or "--test" in sys.argv
+    # INPUT_FILE = f'Day_{DAY}{ "_test" if TEST else "" }.txt'
+    INPUT_FILE = f'Day_{DAY}.txt'
+
+    with open(INPUT_FILE) as F:
+        INPUT = F.read()
+    
+    solution_module = importlib.import_module(f'day{DAY}part{PART}')
+
+    print(solution_module.solution(INPUT))
