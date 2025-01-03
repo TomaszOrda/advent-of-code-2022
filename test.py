34f931bdfc1001b4a34d7957779b3a4afc467a8f
TomaszOrda <tomaszorda@outlook.com>
2025-01-03 12:46:49 +0100

    Add type annotation to ANSWERS

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 import unittest
 import importlib
+from typing import Dict, Tuple, Any
 
 def get_test_file(file):
-    with open(f'data\{file}') as F:
+    with open(f'data\\{file}') as F:
         raw_input = F.read()
     return raw_input
 
 SOLUTIONS_TO_TEST = [(day, part) for day in range(1,26) for part in {1,2} if day in [7] and (day,part) != (25,2)]
-ANSWERS = {
-    (7,1) : 95437,
-    (7,2) : 24933642
+ANSWERS: Dict[Tuple[int, int], Any] = {
+    (7,1): 95437, 
+    (7,2): 24933642, 
 }
 
 class TestSolution(unittest.TestCase):
