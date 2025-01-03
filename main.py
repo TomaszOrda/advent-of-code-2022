fff4d5055286ef491349c4244f92e84debf33729
TomaszOrda <tomaszorda@outlook.com>
2025-01-03 11:14:42 +0100

    Change argument handling for main
    Change from 'main.py dayxparty' to 'main.py x y'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 
 if __name__ == "__main__":
-    DAY, PART = sys.argv[1].removeprefix("day").split("part")
+    DAY, PART = sys.argv[1], sys.argv[2]
     # TEST = "-test" in sys.argv or "--test" in sys.argv
     # INPUT_FILE = f'Day_{DAY}{ "_test" if TEST else "" }.txt'
     INPUT_FILE = f'data\\day_{DAY}.txt'
