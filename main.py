cfd4f91edc37175423b96b7e863dd990625a9963
TomaszOrda <tomaszorda@outlook.com>
2025-01-03 11:15:39 +0100

    Remove unnecessary comments

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import sys
 
 if __name__ == "__main__":
     DAY, PART = sys.argv[1], sys.argv[2]
-    # TEST = "-test" in sys.argv or "--test" in sys.argv
-    # INPUT_FILE = f'Day_{DAY}{ "_test" if TEST else "" }.txt'
     INPUT_FILE = f'data\\day_{DAY}.txt'
 
     with open(INPUT_FILE) as F:
