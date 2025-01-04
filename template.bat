81a04e9a8dfe666019ea10c2c25a62be446ee97b
TomaszOrda <tomaszorda@outlook.com>
2025-01-04 11:24:33 +0100

    Add a script to automate creation of solution files templates

---
+++ template.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set DayNumber=%1
+
+type NUL > data\day_%DayNumber%.txt
+type NUL > data\day_%DayNumber%_test.txt
+
+set FileNameStem=solutions\day_%DayNumber%_part_
+
+echo.>> %FileNameStem%1.py
+echo.>> %FileNameStem%1.py
+echo def solution(raw_input: str):>> %FileNameStem%1.py
+echo.>> %FileNameStem%1.py
+(echo     return 0) >> %FileNameStem%1.py
+
+copy %FileNameStem%1.py %FileNameStem%2.py
