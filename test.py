10f1908b15dd06cf442154aa6b0bd6eb5cadc51a
TomaszOrda <tomaszorda@outlook.com>
2025-01-23 13:38:58 +0100

    Enforce flake8 standard on test.py

--- test.py
+++ test.py
@@ -3,15 +3,17 @@ import importlib
 import json
 import os
 
+
 def get_test_file(file):
     with open(f'data\\{file}', encoding="UTF-8") as f:
         raw_input = f.read()
     return raw_input
 
+
 def get_test_data():
     with open("test_data.json", encoding="UTF-8") as f:
         test_data = json.load(f)
-    return {tuple(map(int, key.split('-'))):answer for (key,answer) in test_data.items()}
+    return {tuple(map(int, key.split('-'))): answer for (key, answer) in test_data.items()}
 
 
 class TestSolution(unittest.TestCase):
@@ -24,6 +26,7 @@ class TestSolution(unittest.TestCase):
             with self.subTest(day=day, part=part):
                 solution_module = importlib.import_module(f'solutions.day_{day}_part_{part}')
                 self.assertIn(solution_module.solution(raw_input), [answer, None])
+
 
 if __name__ == '__main__':
     unittest.main()
